10cda51f2d825956f24d25a4543910bf0f4b4059
Trass3r <un@known.com>
2017-07-28 19:39:44 +0200

    v2.0

--- src/RudeBuildConsole/AssemblyInfo.cs
+++ src/RudeBuildConsole/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RudeBuildConsole")]
-[assembly: AssemblyDescription("A unity C++ build tool for Visual Studio developed by Martin Ecker.")]
+[assembly: AssemblyDescription("A unity C++ build tool for Visual Studio")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Martin Ecker")]
 [assembly: AssemblyProduct("RudeBuildConsole")]
@@ -32,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
